eef4af973cc040cf30c27bb6e13f8ea485abcce7
Joshua Hunsche Jones <joshua@hunschejones.com>
2020-06-05 18:48:44 -0700

    Create, edit, and show blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,23 +2,38 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user, only: [:index, :show]
 
   def index
+    @blogs = Blog.order({ published_at: :desc })
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
+    @blog = Blog.new
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
 
   def create
+    Blog.create!(blogs_params)
+    redirect_to blogs_path
   end
 
   def update
+    @blog = Blog.find(params[:id])
+    @blog.update!(blogs_params)
+    redirect_to blogs_path
   end
 
   def destroy
+  end
+
+  private
+
+  def blogs_params
+    params.require(:blog).permit(:title, :embedded_video, :published_at, :content)
   end
 end
